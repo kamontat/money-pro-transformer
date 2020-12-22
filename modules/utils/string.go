cd00a1d668c48bae0d57fe23c511fc8f8c03be22
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-12-22 18:33:36 +0700

    fix(utils): output string should not use e+<num> format

--- modules/utils/string.go
+++ modules/utils/string.go
@@ -4,5 +4,5 @@ import "strconv"
 
 // FloatToString will convert float64 to string
 func FloatToString(num float64) string {
-	return strconv.FormatFloat(num, 'G', -1, 64)
+	return strconv.FormatFloat(num, 'f', -1, 64)
 }
