47d71d444ff46665199bdbcc05f873549916cdca
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-12-24 17:45:46 +0700

    fix(script): module name changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")/.." || exit 2
 
 export __ROOT_DIRECTORY="$PWD"
 export __COMMANDLINE_DIRECTORY="${__ROOT_DIRECTORY}/modules/cli"
-export __APPLICATION_NAME="money-pro-cli"
+export __APPLICATION_NAME="commandline"
 
 # Build application
 cd "${__COMMANDLINE_DIRECTORY}" || exit 2
@@ -17,7 +17,7 @@ go build || exit 4
 
 # Run built application
 cd "${__ROOT_DIRECTORY}" || exit 2
-"${__COMMANDLINE_DIRECTORY}/${__APPLICATION_NAME}" -rootDir="${__ROOT_DIRECTORY}" "$@"
+"${__COMMANDLINE_DIRECTORY}/${__APPLICATION_NAME}" -rootDir="${__ROOT_DIRECTORY}" "$@" || exit $?
 
 ###########################
 ## Cleanup
