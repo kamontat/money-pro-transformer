29792bf8e3a69dd6f4a8f8f20c9d3200e90d6381
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-12-25 16:39:11 +0700

    refac: support new connection (writer) version

--- modules/csv/go.mod
+++ modules/csv/go.mod
@@ -2,7 +2,7 @@ module moneypro.kamontat.net/utils-csv
 
 go 1.15
 
-replace moneypro.kamontat.net/writer => ../writer
+replace moneypro.kamontat.net/connection-common => ../connection
 
 replace moneypro.kamontat.net/models-common => ../models
 
@@ -17,10 +17,11 @@ replace moneypro.kamontat.net/utils-error => ../error
 replace moneypro.kamontat.net/utils-measure => ../measure
 
 require (
+	moneypro.kamontat.net/connection-common v0.0.0-00010101000000-000000000000
 	moneypro.kamontat.net/models-common v0.0.0-00010101000000-000000000000
+	moneypro.kamontat.net/models-profile v0.0.0-00010101000000-000000000000
 	moneypro.kamontat.net/utils-common v0.0.0-00010101000000-000000000000
 	moneypro.kamontat.net/utils-error v0.0.0-00010101000000-000000000000
 	moneypro.kamontat.net/utils-logger v0.0.0-00010101000000-000000000000
 	moneypro.kamontat.net/utils-measure v0.0.0-00010101000000-000000000000
-	moneypro.kamontat.net/writer v0.0.0-00010101000000-000000000000
 )
--- modules/csv/writer.go
+++ modules/csv/writer.go
@@ -3,19 +3,20 @@ package csv
 import (
 	"strings"
 
+	connection "moneypro.kamontat.net/connection-common"
 	models "moneypro.kamontat.net/models-common"
 	profile "moneypro.kamontat.net/models-profile"
 	e "moneypro.kamontat.net/utils-error"
 	logger "moneypro.kamontat.net/utils-logger"
 	measure "moneypro.kamontat.net/utils-measure"
-	writer "moneypro.kamontat.net/writer"
 )
 
 var logcode = 11000
 
 // Writer is writer for csv format
 type Writer struct {
-	writer.Writer
+	Connection connection.Interface
+	Profile    *profile.Profile
 }
 
 // Header is csv header name
@@ -61,18 +62,18 @@ func (w *Writer) Start(output *logger.Logger) (int, error) {
 
 	timing := measure.NewTiming()
 
-	stepname := "Writing header"
-	size, err := w.Writer.Creator.Write(w.Header(), true)
+	stepname := "      Writing header"
+	size, err := w.Connection.Write(w.Header(), true)
 	if e.When(err).Print(output, 1).Exist() {
 		return byteSize, err
 	}
 	byteSize += size
 	timing.LogSnapshot(stepname, output, logcode+10).Save(stepname)
 
-	stepname = "Writing transaction"
-	w.Writer.Profile.ForEachTransaction(func(index int, transaction *models.Transaction) {
-		csvFormat := Converter(transaction, w.Writer.Profile.GetAccount(transaction.Account), w.Writer.Profile.GetAccount(transaction.AccountTo))
-		size, err := w.Writer.Creator.Write(csvFormat, true)
+	stepname = "      Writing transaction"
+	w.Profile.ForEachTransaction(func(index int, transaction *models.Transaction) {
+		csvFormat := Converter(transaction, w.Profile.GetAccount(transaction.Account), w.Profile.GetAccount(transaction.AccountTo))
+		size, err := w.Connection.Write(csvFormat, true)
 		if e.When(err).Print(output, 1).Empty() {
 			byteSize += size
 		}
@@ -83,12 +84,9 @@ func (w *Writer) Start(output *logger.Logger) (int, error) {
 }
 
 // NewWriter will return create writer object with input value
-func NewWriter(creator *writer.FileCreator, profile *profile.Profile) *Writer {
+func NewWriter(file connection.Interface, profile *profile.Profile) *Writer {
 	return &Writer{
-		writer.Writer{
-			WriterType: writer.CSV,
-			Profile:    profile,
-			Creator:    creator,
-		},
+		Profile:    profile,
+		Connection: file,
 	}
 }
